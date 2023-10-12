41e04a276aad8e9043abd4b309bfe46adac3fa09
Waxanf <147709015+Waxanf@users.noreply.github.com>
2023-10-12 12:46:13 +0200

    Add files via upload

---
+++ Prueba.java
@@ -0,0 +1,9 @@
+public class Prueba
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("Bienvenidos a entornos de desarrollo donde vas a aprender a programar en java");
+        System.out.println(3 + 4);
+    }
+}
